b12110f71da1e95ed2b01c454e0d4193467a8ea5
Omer Misirlioglu <144147956+ahmadien@users.noreply.github.com>
2025-07-04 15:39:18 +0300

    feat(ui): support markdown tables

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeHighlight from 'rehype-highlight'
@@ -8,7 +9,7 @@ import { AiIcon } from './icons/AiIcons'
 export const ChatMessage = ({ message }: { message: Message }) => (
   <div className="py-6">
     <div
-      className={`flex items-start w-full max-w-3xl gap-4 mx-auto rounded-lg p-4 ${
+      className={`chat-message flex items-start w-full max-w-3xl gap-4 mx-auto rounded-lg p-4 ${
         message.role === 'assistant' ? 'bg-[#141414]' : 'bg-[#262626]'
       }`}
       style={
@@ -32,6 +33,7 @@ export const ChatMessage = ({ message }: { message: Message }) => (
       <div className="flex-1 min-w-0 mr-4">
         <ReactMarkdown
           className="prose dark:prose-invert max-w-none"
+          remarkPlugins={[remarkGfm]}
           rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeHighlight]}
         >
           {message.content}
